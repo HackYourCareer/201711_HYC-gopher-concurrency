13c89e8e898873e0b010727a69c71e7c3cba0b9e
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2017-11-28 13:20:30 +0100

    Global-start preparation

--- cmd/site/middleware/global.go
+++ cmd/site/middleware/global.go
@@ -3,21 +3,15 @@ package middleware
 import "net/http"
 
 func NewGlobalLimiter(maxConcurrentUsers int) *GlobalLimiter {
-	return &GlobalLimiter{
-		inProgress: make(chan struct{}, maxConcurrentUsers),
-	}
+	return &GlobalLimiter{}
 }
 
 type GlobalLimiter struct {
-	inProgress chan struct{}
 }
 
 func (l *GlobalLimiter) Limit(next http.HandlerFunc) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
-		l.inProgress <- struct{}{}
 		next(rw, req)
-		<-l.inProgress
-
 	}
 }
 
